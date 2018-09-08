7a6e7eec5b9496fcd042cd979939abb64017f3d8
Mesaque S. Silva <mesaque.s.silva@gmail.com>
2018-09-08 14:39:06 -0300

    new route and k8s structure

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # k8s-v1-admin
+
+## create cluster project
+```sh
+gcloud container clusters create k8s-v1-admin  --labels=environment=production,v=1,operator=mesaquesilva --min-nodes=2 --max-nodes=13 --enable-autoscaling --node-labels=environment=production
+
+
+kubectl create -f k8s/controller/admin-prod.yaml
+kubectl create -f k8s/service/admin-prod.yaml
+
+```
+
--- index.php
+++ index.php
@@ -13,6 +13,12 @@ $app->get('/date/', function ($request, $response, $args) {
     return $response->getBody()->write( date("h:i:sa") );
 });
 
+$app->get('/cryptocurrency/{name}', function ($request, $response, $args) {
+
+	$json = json_decode( file_get_contents( 'https://www.bitstamp.net/api/v2/ticker/'.$args['name'].'/') );
+    return $response->getBody()->write(  $json->bid . '$' );
+});
+
 $app->get('/', function ($request, $response, $args) {
     return $response->getBody()->write( 'This is Home' );
 });
