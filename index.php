542687966185576d0d7598257b9ee0dc4aa29d29
Mesaque S. Silva <mesaque.s.silva@gmail.com>
2018-09-08 17:35:03 -0300

    add comment on index

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * App Sample
+ * 
+ * This App is a sample to show how to use the kubernetes solution for a scalable infrastructure from a versioned code.
+ * 
+ * @author Mesaque Silva <mesaque.s.silva@gmail.com>
+*/
 require 'vendor/autoload.php';
 date_default_timezone_set("America/Sao_Paulo");
 
